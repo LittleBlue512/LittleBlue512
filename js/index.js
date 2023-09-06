629aafe78ad386b3990143340610584bb301b911
Chattipoom <chattipoom.siri@gmail.com>
2023-09-06 22:59:17 +0700

    Add JavaScript

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Greet the playful one who opens the console ~
+console.log(
+  "%cHello, curious one! Welcome to my portfolio site. I hope you enjoy your stay. 😉🍀",
+  "font-size: 1.5rem; margin: 0 0 1rem 0; padding: 1rem; color: #e1e6eb; background-color: #11191f; border-radius: 0.5rem;"
+);
